1ec8a4abe112c59bdcaf59391c57860598a8f77b
Florian Le Frioux <florian@lefrioux.fr>
2010-11-28 13:11:10 +0100

    MAJ: dlShow.php - Ajout gestion ezrss.it

--- dlShow.php
+++ dlShow.php
@@ -11,7 +11,7 @@
  * "Robot" permettant la détection (et le téléchargement) des nouveaux épisodes des séries suivies 
  * depuis le site TVU.org.ru, et entre autres gère l'ajout en BDD de leurs noms et nicknames
  *
- * TODO : Gérer ezrss.it
+ * ! TODO : Vérifier si la gestion ezrss.it est "vraiment" bugless
  * 
 \**************************************************************************************************/
 
@@ -30,10 +30,10 @@ while ($donneesSerie = mysqli_fetch_row($reqSerie))
 	}
 	
 	// Téléchargement des flux ezRSS.it		http://www.ezrss.it/search/index.php?show_name=xxxx&mode=rss
-	elseif (stristr($donnees_serie[0], 'eztv')) {
-		$showID = mb_strrichr($donnees_serie[0], '_eztv', TRUE);
+	elseif (stristr($donnees_serie[0], 'ezrss')) {
+		$showID = mb_strrichr($donnees_serie[0], '_ezrss', TRUE);
 		$rssURL = 'http://www.ezrss.it/search/index.php?show_name='.$showID.'&mode=rss';
-		$showID .= '_eztv';
+		$showID .= '_ezrss';
 	}
 
 	
@@ -49,64 +49,66 @@ while ($donneesSerie = mysqli_fetch_row($reqSerie))
 			$showName = $v->title->asXML();
 			$showName = substr($showName, 39);
 			$showName = stristr($showName,'-',TRUE);
-			$showName = trim($showName); // On trim car il peut contenir un espace au début et a la fin
+			$showName = trim($showName); // On récupère le nom de la série
 		}
 
 		foreach ($xml->channel->item as $v) { 
 		//<title>[torrent] Dexter - 4x12 - The Getaway</title>     
 		//<guid>http://tvunderground.org.ru/torrent.php?tid=2356</guid>
 			
-			$showEpisode = $v->title->asXML();
-			$showEpisode = mb_stristr($showEpisode, ' - ');
-			$showEpisode = substr($showEpisode, 3);
-			$showEpisode = mb_stristr($showEpisode, ' - ', TRUE);
-			$ar_showEpisode[] = $showEpisode;
+			$SaisonEpisode = $v->title->asXML();
+			$SaisonEpisode = mb_stristr($SaisonEpisode, ' - ');
+			$SaisonEpisode = substr($SaisonEpisode, 3);
+			$SaisonEpisode = mb_stristr($SaisonEpisode, ' - ', TRUE);
+			$ar_SaisonEpisode[] = $SaisonEpisode;	// On récupère le numéro de la saison et de l'episode
 			
 			$tid = $v->guid->asXML();
 			$tid = substr($tid, 50, -7);
 			$tid .= '_tvu';	
-			$ar_tid[] = $tid;
+			$ar_tid[] = $tid;	// On récupère l'identifiant du torrent de l'episode, qui sert aussi d'ID "unique" de l'episode
 		}
 	}
-	elseif (stristr($rssURL, 'ezrss.it')) { // ! TODO Récuprération des données pour ezrss.it
+	elseif (stristr($rssURL, 'ezrss.it')) {
 	
 		foreach ($xml->channel->item as $v) { 
 		//<link>http://torrent.zoink.it/Dexter.S05E09.HDTV.XviD-FEVER.[eztv].torrent</link>
 		//<category domain="http://eztv.it/shows/78/dexter/"><![CDATA[TV Show / Dexter]]></category>
 		//<description><![CDATA[Show Name: Dexter; Episode Title: N/A; Season: 5; Episode: 9]]></description>
+		//<comments>http://eztv.it/forum/discuss/24173/</comments>
 		
-		$link = $v->link->asXML();
-		$link = substr($link, 6, -7);
-		$ar_link[] = $link;
+			$url = $v->link->asXML();
+			$url = substr($url, 6, -7);
+			$ar_url[] = $url;		// On récupère le lien du torrent de l'episode
 		
-		$showName = $v->category->asXML();
-		$showName = mb_stristr($showName,'/ ');
-		$showName = substr($showName, 2,-14);
+			$showName = $v->category->asXML();
+			$showName = mb_stristr($showName,'/ ');
+			$showName = substr($showName, 2,-14);	// On récupère le nom de la série
 		
-		$ligne = $v->description->asXML();
-		$nbSaison = stristr($ligne, 'Season: ');
-		$nbSaison = substr($nbSaison, 8);
-		$nbSaison = stristr($nbSaison, '; Ep', TRUE);	// On récupère le numéro de saison
+			$ligne = $v->description->asXML();
+			$nbSaison = stristr($ligne, 'Season: ');
+			$nbSaison = substr($nbSaison, 8);
+			$nbSaison = stristr($nbSaison, '; Ep', TRUE);	// On récupère le numéro de la saison
 						
-		$nbEpisode = stristr($ligne,'Episode: ');
-		$nbEpisode = substr($nbEpisode, 9);
-		$nbEpisode = stristr($nbEpisode, ']]>', TRUE);	// On récupère le numéro de l'épisode
+			$nbEpisode = stristr($ligne,'Episode: ');
+			$nbEpisode = substr($nbEpisode, 9);
+			$nbEpisode = stristr($nbEpisode, ']]>', TRUE);	// On récupère le numéro de l'episode
 		
-		$SaisonEpisode = $nbSaison.'x'.$nbEpisode;
-		$ar_SaisonEpisode[] = $SaisonEpisode;
+			$SaisonEpisode = $nbSaison.'x'.$nbEpisode;
+			$ar_SaisonEpisode[] = $SaisonEpisode;
 		
-		$tid = $v->comments->asXML();
-		$tid = stristr($tid,'discuss/');
-		$tid = substr($tid, 8);
-		$tid = stristr($tid, '/</comments>', TRUE);
-		e$tid .= '_eztv';		// On récupère l'identifiant "unique" de l'episode
-		$ar_tid[] = $tid;
+			$tid = $v->comments->asXML();
+			$tid = stristr($tid,'discuss/'); 
+			$tid = substr($tid, 8);
+			$tid = stristr($tid, '/</comments>', TRUE);
+			$tid .= '_ezrss';								// On récupère l'identifiant "unique" de l'episode
+			if ($tid != '_ezrss') {
+				$ar_tid[] = $tid;
+			}
 		}
 	}
 	
 	
 	/* * * * Traitements des différentes données récupérées * * * */	
-	// ! TODO Traitements pour ezrss.it
 	
 	// Traitement des torrentIDs
 		
@@ -119,23 +121,30 @@ while ($donneesSerie = mysqli_fetch_row($reqSerie))
 			while ($donneesTID = mysqli_fetch_row($req_oldTID)) 
 			{
 				if ($donneesTID[0] != 0) {
-					$oldTID = tri_selonSite($donneesTID[0], 1);
+					$oldTID = tri_selonSite($donneesTID[0]);
 				}
 				else $oldTID = '0';
 				
-				$newTID = tri_selonSite($tid, 1); 
+				$newTID = tri_selonSite($tid); 
 			
-				if ($newTID > $oldTID) 			// $old et $new sont des entiers (ne comprenant pas '_tvu' / '_eztv') contrairement a $donneesTID[0] et $tid respectivement
+				if ($newTID > $oldTID) 			// $oldTID et $newTID sont des entiers (ne comprenant pas '_tvu' / '_ezrss') contrairement a $donneesTID[0] et $tid respectivement
 				{				
 					if (stristr($showID, '_tvu')) {					
 						$torURL = 'http://tvunderground.org.ru/torrent.php?tid='.$newTID; // http://tvunderground.org.ru/torrent.php?tid=xxxxxx 	URL Torrent
+						$torLocal = '/home/floweb/newtorrent/torrent_'.$newTID.'.torrent'; 
+						$cmdTor = '/usr/bin/wget -O '.$torLocal.' '.$torURL;
+						exec($cmdTor);
 					}
-					elseif (stristr($showID, '_eztv')) {
-						// ! TODO Traitement pour ezrss.it
+					elseif (stristr($showID, '_ezrss')) {
+						asort($ar_url);
+						foreach ($ar_url as $key => $torURL) {
+							$torLocal = '/home/floweb/newtorrent/torrent_'.$newTID.'.torrent'; 
+							$cmdTor = '/usr/bin/wget -O '.$torLocal.' '.$torURL;
+							exec($cmdTor);
+							unset($ar_url[$torURL]);
+							break;
+						}
 					}
-					$tor_local = '/home/floweb/newtorrent/torrent_'.$newTID.'.torrent'; 
-					$cmd_tor = '/usr/bin/wget -O '.$tor_local.' '.$torURL;
-					exec($cmd_tor);
 					mysqli_query($link, "UPDATE serie SET old_tid = '".$tid."' WHERE show_id = '".$showID."'") or exit(mysqli_error($link));
 				}
 			}
@@ -147,12 +156,12 @@ while ($donneesSerie = mysqli_fetch_row($reqSerie))
 		
 	// Traitement pour le numéro du dernier épisode téléchargé
 		
-	if (count($ar_showEpisode) >= 1) {
-		asort($ar_showEpisode);
-		foreach ($ar_showEpisode as $key => $last_ep) {
-			mysqli_query($link, "UPDATE serie SET last_episode = '".$last_ep."' WHERE show_id = '".$showID."'") or exit(mysqli_error($link));					
+	if (count($ar_SaisonEpisode) >= 1) {
+		asort($ar_SaisonEpisode);
+		foreach ($ar_SaisonEpisode as $key => $lastEp) {
+			mysqli_query($link, "UPDATE serie SET last_episode = '".$lastEp."' WHERE show_id = '".$showID."'") or exit(mysqli_error($link));					
 		}
-		unset($ar_showEpisode);
+		unset($ar_SaisonEpisode);
 	}
 		
 		
